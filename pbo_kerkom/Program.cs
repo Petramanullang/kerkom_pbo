573b54d31075323fd4a0f5eff72acb1ca5aa0fd3
Petramanullang <petra.mainacc@gmail.com>
2026-01-02 09:00:10 +0700

    Add project files.

---
+++ pbo_kerkom/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace pbo_kerkom
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
