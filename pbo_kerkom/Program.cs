ffba71294973937761d5e39e1973c8dc4e02bde0
Petramanullang <petra.mainacc@gmail.com>
2026-01-02 17:48:31 +0700

    add : FileServices.cs, finish writing the code

--- pbo_kerkom/Program.cs
+++ pbo_kerkom/Program.cs
@@ -1,10 +1,33 @@
-﻿namespace pbo_kerkom
+﻿using System;
+using System.Collections.Generic;
+using pbo_kerkom.Interfaces;
+using pbo_kerkom.Models;
+using pbo_kerkom.Services;
+
+namespace pbo_kerkom
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			try
+			{
+				List<IKaryawan> daftarKaryawan = new List<IKaryawan>
+				{
+					new Staf("Andi", 3000000),
+					new Supervisor("Budi", 5000000),
+					new Manajer("Citra", 8000000)
+				};
+
+				FileService.SimpanKeFile("dataKaryawan.txt", daftarKaryawan);
+
+				Console.WriteLine("Data karyawan berhasil disimpan ke file.");
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine("Terjadi kesalahan:");
+				Console.WriteLine(ex.Message);
+			}
+		}
+	}
 }
